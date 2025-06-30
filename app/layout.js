44c17dbd7d1b75ecbe1052588fba5c63d0432fd4
Mint-th <1x909kohdyi@gmail.com>
2025-06-30 11:16:10 +0700

    1

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navigation from "./conponents/Navigation";
 
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
